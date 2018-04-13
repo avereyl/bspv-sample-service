db4c0e9ddceeaea157ff5fc3e3292e809bf7e850
Guillaume Billaud <guillaume.billaud@dachser.com>
2018-04-13 16:42:53 +0200

    Starting sample-service project

---
+++ src/main/java/org/bspv/sample/BspvSampleServiceApplication.java
@@ -0,0 +1,12 @@
+package org.bspv.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BspvSampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BspvSampleServiceApplication.class, args);
+	}
+}
