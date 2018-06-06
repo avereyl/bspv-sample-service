81491234b4e4ff3f8766f9ff0cbdec08929aae8f
avereyl <guillaume.billaud@gmail.com>
2018-06-06 18:05:13 +0200

    starting to implement oauth2

--- src/main/java/org/bspv/sample/BspvSampleServiceApplication.java
+++ src/main/java/org/bspv/sample/BspvSampleServiceApplication.java
@@ -1,5 +1,6 @@
 package org.bspv.sample;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
+@EnableResourceServer
 @SpringBootApplication
 public class BspvSampleServiceApplication {
 
@@ -32,4 +38,24 @@ public class BspvSampleServiceApplication {
 	        return this.discoveryClient.getInstances(applicationName);
 	    }
     }
+    
+    @RestController
+    @RequestMapping("/")
+    class DummyController {
+
+        @RequestMapping(method = RequestMethod.GET)
+        @ResponseBody
+        public String helloWorld(Principal principal) {
+            return principal == null ? "Hello anonymous" : "Hello " + principal.getName();
+        }
+
+        @PreAuthorize("#oauth2.hasScope('openid') and hasRole('ROLE_ADMIN')")
+        @RequestMapping(value = "secret", method = RequestMethod.GET)
+        @ResponseBody
+        public String helloSecret(Principal principal) {
+            return principal == null ? "Hello anonymous" : "S3CR3T  - Hello " + principal.getName();
+        }
+    }
+    
+    
 }
