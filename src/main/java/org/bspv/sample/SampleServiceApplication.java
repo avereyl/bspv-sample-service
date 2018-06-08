dc71c3048b13ece3feff39584e33e4e77be93da2
avereyl <guillaume.billaud@gmail.com>
2018-06-08 16:50:44 +0200

    Backup commit

--- src/main/java/org/bspv/sample/SampleServiceApplication.java
+++ src/main/java/org/bspv/sample/SampleServiceApplication.java
@@ -1,44 +1,26 @@
 package org.bspv.sample;
 
 import java.security.Principal;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
-@EnableResourceServer
 @SpringBootApplication
-public class BspvSampleServiceApplication {
+public class SampleServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BspvSampleServiceApplication.class, args);
-    }
-
-    @RestController
-	class ServiceInstanceRestController {
-
-	    @Autowired
-	    private DiscoveryClient discoveryClient;
-
-	    @RequestMapping("/service-instances/{applicationName}")
-	    public List<ServiceInstance> serviceInstancesByApplicationName(
-	            @PathVariable String applicationName) {
-	        return this.discoveryClient.getInstances(applicationName);
-	    }
+        SpringApplication.run(SampleServiceApplication.class, args);
     }
     
+   
+    //FIXME TO MOVE
     @RestController
     @RequestMapping("/")
     class DummyController {
@@ -56,6 +38,8 @@ public class BspvSampleServiceApplication {
             return principal == null ? "Hello anonymous" : "S3CR3T  - Hello " + principal.getName();
         }
     }
+    
+    
     
     
 }
--- src/test/java/org/bspv/sample/SampleServiceApplicationTests.java
+++ src/test/java/org/bspv/sample/SampleServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BspvSampleServiceApplicationTests {
+public class SampleServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
